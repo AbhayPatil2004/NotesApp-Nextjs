47f7a8d75e1a73f3b016aaf0baf38f0ffbdcf49a
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2025-11-20 18:37:14 +0530

    Pdf fetched Succesfull

---
+++ src/app/api/fetchPdfs/route.ts
@@ -0,0 +1,31 @@
+// app/api/fetchImages/route.ts
+import { NextResponse } from "next/server";
+import dbConnect from "@/dbConnect/db";
+import Pdf from "../../../models/pdfModel";
+
+export async function GET() {
+  try {
+    await dbConnect();
+    console.log("Database connected");
+
+    const data = await Pdf.find().lean(); // .lean() returns plain objects
+
+    if (!data || data.length === 0) {
+      return NextResponse.json(
+        { ok: true, message: "No images found", images: [] },
+        { status: 200 }
+      );
+    }
+
+    return NextResponse.json(
+      { ok: true, message: "Images fetched successfully", images: data },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.error("GET /api/fetchImages error:", error);
+    return NextResponse.json(
+      { ok: false, message: `Error fetching images: ${error?.message || error}` },
+      { status: 500 }
+    );
+  }
+}
